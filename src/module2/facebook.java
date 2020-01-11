2daad67bd746b490f8eefbe822347a911d3cf879
kpenukula <kpenukula@kalyans-mbp-3.attlocal.net>
2020-01-11 10:48:58 -0600

    this is all files

--- src/module2/facebook.java
+++ src/module2/facebook.java
@@ -1,10 +1,31 @@
 package module2;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class facebook {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws InterruptedException {
 		// TODO Auto-generated method stub
 
-	}
-
+		
+/*	System.setProperty("webdriver.chrome.driver", "Firefoxdriver");
+        WebDriver driver = new FirefoxDriver();
+     driver.manage().window().maximize(); 
+       
+        driver.get("http://facebook.com"); */
+		
+		
+	
+		System.setProperty("webdriver.chrome.driver", "D: \\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.guru99.com/selenium/guru99home/");
+		driver.manage().window().maximize();
+		driver.navigate().refresh();
+		driver.close();
+		
+		
+		
+		
 }
+	}
